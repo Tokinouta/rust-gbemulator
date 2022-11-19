02f9301d120eda60e1ec33ed6a8b5d33c5607699
Hou Boyao <shangrila-1100@hotmail.com>
2022-11-19 17:01:44 +0800

    added loading instructions

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1,7 +1,317 @@
-use self::register::Register;
+use crate::memory::Memory;
+
+use self::register::{Flag, Register};
 
 mod register;
 
 struct Cpu {
     register: Register,
+    memory: Memory,
+    opcode: u8,
+}
+
+impl Cpu {
+    pub fn new() -> Self {
+        Self {
+            register: Register::new(),
+            memory: Memory::new(),
+            opcode: 0,
+        }
+    }
+
+    fn imm8(&mut self) -> u8 {
+        let imm8 = self.memory.get8(self.register.get_pc());
+        self.register.pc_inc(1);
+        imm8
+    }
+
+    fn imm16(&mut self) -> u16 {
+        let imm16 = self.memory.get16(self.register.get_pc());
+        self.register.pc_inc(2);
+        imm16
+    }
+
+    pub fn ld(&mut self, opcode: u8) {
+        match opcode {
+            // ld nn,n 8 bit immediate
+            0x06 => {
+                let n = self.imm8();
+                self.register.set_b(n)
+            }
+            0x0e => {
+                let n = self.imm8();
+                self.register.set_c(n)
+            }
+            0x16 => {
+                let n = self.imm8();
+                self.register.set_d(n)
+            }
+            0x1e => {
+                let n = self.imm8();
+                self.register.set_e(n)
+            }
+            0x26 => {
+                let n = self.imm8();
+                self.register.set_h(n)
+            }
+            0x2e => {
+                let n = self.imm8();
+                self.register.set_l(n)
+            }
+            0x36 => {
+                let n = self.imm8();
+                self.memory.set8(self.register.get_hl(), n)
+            }
+            0x3e => {
+                let n = self.imm8();
+                self.register.set_a(n)
+            }
+
+            // LD n,nn 16 bit immediate
+            0x01 => {
+                let n = self.imm16();
+                self.register.set_bc(n)
+            }
+            0x11 => {
+                let n = self.imm16();
+                self.register.set_de(n)
+            }
+            0x21 => {
+                let n = self.imm16();
+                self.register.set_hl(n)
+            }
+            0x31 => {
+                let n = self.imm16();
+                self.register.set_sp(n)
+            }
+
+            // ld r1, r2
+            0x7f => self.register.set_a(self.register.get_a()),
+            0x78 => self.register.set_a(self.register.get_b()),
+            0x79 => self.register.set_a(self.register.get_c()),
+            0x7a => self.register.set_a(self.register.get_d()),
+            0x7b => self.register.set_a(self.register.get_e()),
+            0x7c => self.register.set_a(self.register.get_h()),
+            0x7d => self.register.set_a(self.register.get_l()),
+
+            0x40 => self.register.set_b(self.register.get_b()),
+            0x41 => self.register.set_b(self.register.get_c()),
+            0x42 => self.register.set_b(self.register.get_d()),
+            0x43 => self.register.set_b(self.register.get_e()),
+            0x44 => self.register.set_b(self.register.get_h()),
+            0x45 => self.register.set_b(self.register.get_l()),
+            0x47 => self.register.set_b(self.register.get_a()),
+
+            0x48 => self.register.set_c(self.register.get_b()),
+            0x49 => self.register.set_c(self.register.get_c()),
+            0x4a => self.register.set_c(self.register.get_d()),
+            0x4b => self.register.set_c(self.register.get_e()),
+            0x4c => self.register.set_c(self.register.get_h()),
+            0x4d => self.register.set_c(self.register.get_l()),
+            0x4f => self.register.set_c(self.register.get_a()),
+
+            0x50 => self.register.set_d(self.register.get_b()),
+            0x51 => self.register.set_d(self.register.get_c()),
+            0x52 => self.register.set_d(self.register.get_d()),
+            0x53 => self.register.set_d(self.register.get_e()),
+            0x54 => self.register.set_d(self.register.get_h()),
+            0x55 => self.register.set_d(self.register.get_l()),
+            0x57 => self.register.set_d(self.register.get_a()),
+
+            0x58 => self.register.set_e(self.register.get_b()),
+            0x59 => self.register.set_e(self.register.get_c()),
+            0x5a => self.register.set_e(self.register.get_d()),
+            0x5b => self.register.set_e(self.register.get_e()),
+            0x5c => self.register.set_e(self.register.get_h()),
+            0x5d => self.register.set_e(self.register.get_l()),
+            0x5f => self.register.set_e(self.register.get_a()),
+
+            0x60 => self.register.set_h(self.register.get_b()),
+            0x61 => self.register.set_h(self.register.get_c()),
+            0x62 => self.register.set_h(self.register.get_d()),
+            0x63 => self.register.set_h(self.register.get_e()),
+            0x64 => self.register.set_h(self.register.get_h()),
+            0x65 => self.register.set_h(self.register.get_l()),
+            0x67 => self.register.set_h(self.register.get_a()),
+
+            0x68 => self.register.set_l(self.register.get_b()),
+            0x69 => self.register.set_l(self.register.get_c()),
+            0x6a => self.register.set_l(self.register.get_d()),
+            0x6b => self.register.set_l(self.register.get_e()),
+            0x6c => self.register.set_l(self.register.get_h()),
+            0x6d => self.register.set_l(self.register.get_l()),
+            0x6f => self.register.set_l(self.register.get_a()),
+
+            // LD from/to memory
+            0x0a => self
+                .register
+                .set_a(self.memory.get8(self.register.get_bc())),
+            0x1a => self
+                .register
+                .set_a(self.memory.get8(self.register.get_de())),
+            0x7e => self
+                .register
+                .set_a(self.memory.get8(self.register.get_hl())),
+            0x46 => self
+                .register
+                .set_b(self.memory.get8(self.register.get_hl())),
+            0x4e => self
+                .register
+                .set_c(self.memory.get8(self.register.get_hl())),
+            0x56 => self
+                .register
+                .set_d(self.memory.get8(self.register.get_hl())),
+            0x5e => self
+                .register
+                .set_e(self.memory.get8(self.register.get_hl())),
+            0x66 => self
+                .register
+                .set_h(self.memory.get8(self.register.get_hl())),
+            0x6e => self
+                .register
+                .set_l(self.memory.get8(self.register.get_hl())),
+            0x02 => self
+                .memory
+                .set8(self.register.get_bc(), self.register.get_a()),
+            0x12 => self
+                .memory
+                .set8(self.register.get_de(), self.register.get_a()),
+            0x70 => self
+                .memory
+                .set8(self.register.get_hl(), self.register.get_b()),
+            0x71 => self
+                .memory
+                .set8(self.register.get_hl(), self.register.get_c()),
+            0x72 => self
+                .memory
+                .set8(self.register.get_hl(), self.register.get_d()),
+            0x73 => self
+                .memory
+                .set8(self.register.get_hl(), self.register.get_e()),
+            0x74 => self
+                .memory
+                .set8(self.register.get_hl(), self.register.get_h()),
+            0x75 => self
+                .memory
+                .set8(self.register.get_hl(), self.register.get_l()),
+            0x77 => self
+                .memory
+                .set8(self.register.get_hl(), self.register.get_a()),
+
+            0xfa => {
+                let n = self.imm16();
+                self.register.set_a(self.memory.get8(n))
+            }
+            0xea => {
+                let n = self.imm16();
+                self.memory.set8(n, self.register.get_a())
+            }
+
+            // LD A,(C)
+            0xf2 => self
+                .register
+                .set_a(self.memory.get8(0xff00 + self.register.get_c() as u16)),
+            // LD (C),A
+            0xe2 => self
+                .memory
+                .set8(0xff00 + self.register.get_c() as u16, self.register.get_a()),
+            // LDI A,(HL)
+            0x2a => {
+                let temp = self.register.get_hl();
+                self.register.set_a(self.memory.get8(temp));
+                self.register.set_hl(temp + 1);
+            }
+            // LDI (HL),A
+            0x22 => {
+                let temp = self.register.get_hl();
+                self.memory.set8(temp, self.register.get_a());
+                self.register.set_hl(temp + 1);
+            }
+            // LDD A,(HL)
+            0x3a => {
+                let temp = self.register.get_hl();
+                self.register.set_a(self.memory.get8(temp));
+                self.register.set_hl(temp - 1);
+            }
+            // LDD (HL),A
+            0x32 => {
+                let temp = self.register.get_hl();
+                self.memory.set8(temp, self.register.get_a());
+                self.register.set_hl(temp - 1);
+            }
+            // LDH (n),A
+            0xe0 => {
+                let n = self.imm8();
+                self.memory.set8(0xff00 + n as u16, self.register.get_a())
+            }
+            // LDH A,(n)
+            0xf0 => {
+                let n = self.imm8();
+                self.register.set_a(self.memory.get8(0xff00 + n as u16))
+            }
+
+            // LD SP,HL
+            0xf9 => self.register.set_sp(self.register.get_hl()),
+            // LD HL,SP+n
+            0xf8 => {
+                let a = self.register.get_sp();
+                let b = self.imm8() as i8 as i16 as u16;
+                let flag = !Flag::Z
+                    | !Flag::N
+                    | if (a & 0x00ff) + (b & 0x00ff) > 0x00ff {
+                        Flag::C
+                    } else {
+                        !Flag::C
+                    }
+                    | if (a & 0x000f) + (b & 0x000f) > 0x000f {
+                        Flag::H
+                    } else {
+                        !Flag::H
+                    };
+                self.register.set_flag(flag);
+                self.register.set_hl(a.wrapping_add(b));
+            }
+            // LD (nn),SP
+            0x08 => {
+                let n = self.imm16();
+                self.memory.set16(self.register.get_sp(), n)
+            }
+
+            // PUSH nn
+            0xf5 => self.push(self.register.get_af()),
+            0xc5 => self.push(self.register.get_bc()),
+            0xd5 => self.push(self.register.get_de()),
+            0xe5 => self.push(self.register.get_hl()),
+
+            // POP nn
+            0xf1 => {
+                self.register
+                    .set_af(self.memory.get16(self.register.get_sp()));
+                self.register.set_sp(self.register.get_sp() + 2)
+            }
+            0xc1 => {
+                self.register
+                    .set_bc(self.memory.get16(self.register.get_sp()));
+                self.register.set_sp(self.register.get_sp() + 2)
+            }
+            0xd1 => {
+                self.register
+                    .set_de(self.memory.get16(self.register.get_sp()));
+                self.register.set_sp(self.register.get_sp() + 2)
+            }
+            0xe1 => {
+                self.register
+                    .set_hl(self.memory.get16(self.register.get_sp()));
+                self.register.set_sp(self.register.get_sp() + 2)
+            }
+            _ => (),
+        }
+    }
+
+    #[inline]
+    pub fn push(&mut self, n: u16) {
+        self.memory.set16(self.register.get_sp(), n);
+        self.register.set_sp(self.register.get_sp() - 2)
+    }
+
 }
--- src/cpu/register.rs
+++ src/cpu/register.rs
@@ -16,7 +16,7 @@ pub struct Register {
 bitflags! {
     pub struct Flag: u8 {
         const Z =  0x80; // zero
-        const O =  0x40; // operation
+        const N =  0x40; // operation
         const H =  0x20; // half-carry
         const C =  0x10; // carry
     }
@@ -28,6 +28,100 @@ impl Register {
         Self { a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, h: 0, l: 0, pc: 0x100, sp: 0xfffe }
     }
 
+    pub fn get_flag(&self) -> Flag {
+        Flag::from_bits_truncate(self.f)
+    }
+
+    pub fn set_flag(&mut self, flag: Flag) {
+        self.f |= flag.bits;
+    }
+}
+
+// 一位寄存器访存
+impl Register {
+    pub fn get_a(&self) -> u8 {
+        self.a
+    }
+
+    pub fn get_b(&self) -> u8 {
+        self.b
+    }
+
+    pub fn get_c(&self) -> u8 {
+        self.c
+    }
+
+    pub fn get_d(&self) -> u8 {
+        self.d
+    }
+
+    pub fn get_e(&self) -> u8 {
+        self.e
+    }
+
+    pub fn get_f(&self) -> u8 {
+        self.f
+    }
+
+    pub fn get_h(&self) -> u8 {
+        self.h
+    }
+
+    pub fn get_l(&self) -> u8 {
+        self.l
+    }
+
+    pub fn get_pc(&self) -> u16 {
+        self.pc
+    }
+
+    pub fn get_sp(&self) -> u16 {
+        self.sp
+    }
+
+    pub fn set_a(&mut self, n: u8) {
+        self.a = n
+    }
+
+    pub fn set_b(&mut self, n: u8) {
+        self.b = n
+    }
+
+    pub fn set_c(&mut self, n: u8) {
+        self.c = n
+    }
+
+    pub fn set_d(&mut self, n: u8) {
+        self.d = n
+    }
+
+    pub fn set_e(&mut self, n: u8) {
+        self.e = n
+    }
+
+    pub fn set_f(&mut self, n: u8) {
+        self.f = n
+    }
+
+    pub fn set_h(&mut self, n: u8) {
+        self.h = n
+    }
+
+    pub fn set_l(&mut self, n: u8) {
+        self.l = n
+    }
+
+    pub fn set_sp(&mut self, n: u16) {
+        self.sp = n
+    }
+
+    pub fn pc_inc(&mut self, n: u16) {
+        self.pc += n;
+    }
+}
+
+// 两位寄存器访存
+impl Register {
     pub fn get_af(&self) -> u16 {
         (u16::from(self.a) << 8) | u16::from(self.f)
         // 从左到右是高字节到低字节，但是这个操作可能需要从内存里读好几次，比较慢
@@ -44,10 +138,6 @@ impl Register {
 
     pub fn get_hl(&self) -> u16 {
         (u16::from(self.h) << 8) | u16::from(self.l)
-    }
-
-    pub fn get_flag(&self) -> Flag {
-        Flag::from_bits_truncate(self.f)
     }
 
     pub fn set_af(&mut self, op: u16) {
@@ -68,9 +158,5 @@ impl Register {
     pub fn set_hl(&mut self, op: u16) {
         self.h = (op >> 8) as u8;
         self.l = (op & 0x00ff) as u8;
-    }
-
-    pub fn set_flag(&mut self, flag: Flag) {
-        self.f |= flag.bits;
     }
 }
